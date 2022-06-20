9c7d9d2017005c2f021f0f73df5d276c5a1554ac
John Ewart <johnewart@microsoft.com>
2022-06-19 18:04:46 -0700

    Initial commit

---
+++ home.nix
@@ -0,0 +1,57 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "johnewart";
+  home.homeDirectory = "/home/johnewart";
+ 
+
+  home.packages = [     
+    pkgs.htop
+    pkgs.fortune
+    pkgs.zsh
+  ];
+
+  programs.git = {
+    enable = true;
+    userName = "John Ewart";
+    userEmail = "johnewart@microsoft.com";
+  };
+
+  programs.zsh = {
+    enable = true;
+    shellAliases = {
+      ll = "ls -l";
+      update = "sudo nixos-rebuild switch";
+    };
+    history = {
+      size = 10000;
+      path = "${config.xdg.dataHome}/zsh/history";
+    };
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" "thefuck" ];
+      theme = "robbyrussell";
+    };
+  };
+
+  services.gpg-agent = { 
+    enable = true;
+    defaultCacheTtl = 1800;
+    enableSshSupport = true;
+  };
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.05";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
